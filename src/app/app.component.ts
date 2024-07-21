9790ddf1e7924933800012e497b31f5b85d32936
frankxhunter <frankxhunter@gmail.com>
2024-07-21 21:04:17 +0200

    First commit, still is a prototype

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { LateralBarComponent } from './lateral-bar/lateral-bar.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MatSlideToggleModule, LateralBarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
